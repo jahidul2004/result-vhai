ceb01f43652bc37d7a5a1edbb7a268394bd913c5
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-27 11:20:43 +0600

    Result analysis deleted.

--- src/layout/home/Home.jsx
+++ src/layout/home/Home.jsx
@@ -1,13 +1,9 @@
 import Banner from "../../pages/Banner/Banner";
 import CommentSection from "../../pages/commentSection/CommentSection";
-import ResultAnalysis from "../../pages/resultAnalysis/ResultAnalysis";
-
 const Home = () => {
     return (
         <>
             <Banner></Banner>
-            
-            <ResultAnalysis></ResultAnalysis>
 
             <CommentSection></CommentSection>
         </>
